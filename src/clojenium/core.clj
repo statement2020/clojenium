4cf87aa80edb7d7c0e49ef8446cf2ccf1dc61f77
Jamie Bowdidge <jamie@jamies-mbp.home>
2014-05-31 22:48:46 +0100

    Added some initial selenium interop

--- src/clojenium/core.clj
+++ src/clojenium/core.clj
@@ -1,6 +1,62 @@
-(ns clojenium.core)
+(ns clojenium.core
+  (:import (org.openqa.selenium.chrome ChromeDriver)
+           (org.openqa.selenium.firefox FirefoxDriver)
+           (org.openqa.selenium.ie InternetExplorerDriver)
+           (org.openqa.selenium.remote RemoteWebDriver)
+           (org.openqa.selenium WebElement By)))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn click
+  [^WebElement element]
+  (.click element))
+
+(defn clear
+  [^WebElement element]
+  (.clear element))
+
+(defn send-keys
+  [^WebElement element keys]
+  (.sendKeys element keys))
+
+(def kw->by
+  {:css #(By/cssSelector %)
+   :id #(By/id %)
+   :link-text #(By/id %)
+   :class-name #(By/className %)
+   :name #(By/name %)
+   :partial-link-tet #(By/partialLinkText %)
+   :tag-name #(By/tagName %)
+   :x-path #(By/xpath %)})
+
+(defn find-element
+  "Takes a driver, a by and a selector, and returns
+  a WebElement. Default is by css e.g.
+  (find-element driver \"#input\") =>
+  (WebElement id=\"input\")
+
+  (find-element driver :id \"input\") =>
+   (WebElement id=\"input\")"
+  ([driver selector]
+   (find-element driver :css selector))
+  ([driver by selector]
+   (.findElement driver (kw->by by) selector)))
+
+(defn set-driver!
+  [kw]
+  {:chrome (ChromeDriver.)
+   :firefox (FirefoxDriver.)
+   :ie (InternetExplorerDriver.)})
+
+(defn navigate-to
+  [^RemoteWebDriver driver url]
+  (.get driver url))
+
+(defn open-driver
+  "Takes a driver kw, and a url, navigates to the url,
+  and returns the driver for future use."
+  [kw url]
+  (let [driver (set-driver! kw)]
+    (-> driver
+        (navigate-to url))
+    driver))
+
+
